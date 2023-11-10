b6127cf577c3c552623ba2b1b94fd7a205589c01
MatPedu <114214482+MatPedu@users.noreply.github.com>
2023-11-10 09:44:43 +0100

    Create update_variable.php

---
+++ website_files/update_variable.php
@@ -0,0 +1,73 @@
+<?php
+
+$servername = "localhost";
+$dBUsername = "id21525238_id21476219_ismaelfrei";
+$dBPassword = "FASTIoT_2023";
+$dBName = "id21525238_id21476219_esp32";
+
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+
+if (!$conn) {
+	die("Connection failed: ".mysqli_connect_error());
+}
+
+// Check if form is submitted - MODE
+if (isset($_POST['change_mode'])) {
+    $new_mode = $_POST['operation_mode'];
+    $update = mysqli_query($conn, "UPDATE Operation_Mode SET status = '$new_mode' WHERE id = 1;");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the mode value sent from JavaScript
+    $mode = $_POST['mode'];
+
+    if ($mode == 'AUTOMATIC') {
+        $update = mysqli_query($conn, "UPDATE AUTOMATIC SET status = 1 WHERE id = 1;");
+        
+    } else if ($mode == 'SPRAY_NOW') {
+        $update = mysqli_query($conn, "UPDATE Spray_Now SET status = 1 WHERE id = 1;");
+
+    } else if ($mode == 'SCHEDULED') {
+        $update = mysqli_query($conn, "UPDATE SCHEDULED SET status = 1 WHERE id = 1;");
+        
+    } 
+
+    // Perform database update using $mode value (you should have your database connection here)
+    // Example:
+    // $db->query("UPDATE your_table SET mode='$mode' WHERE id=your_id");
+
+    // Return a success message (this will be sent back to the JavaScript as a response)
+    echo 'Database updated successfully';
+} else {
+    echo 'Invalid request';
+}
+
+
+if (isset($_POST['toggle_LED'])) {
+	$sql = "SELECT * FROM LED_status;";
+	$result   = mysqli_query($conn, $sql);
+	$row  = mysqli_fetch_assoc($result);
+	
+	if($row['status'] == 0){
+		$update = mysqli_query($conn, "UPDATE LED_status SET status = 1 WHERE id = 1;");		
+	}		
+	else{
+		$update = mysqli_query($conn, "UPDATE LED_status SET status = 0 WHERE id = 1;");		
+	}
+}
+
+
+
+// Fetch the current operation mode
+$sqlO = "SELECT * FROM Operation_Mode WHERE id = 1;";
+$resultO = mysqli_query($conn, $sqlO);
+$rowO = mysqli_fetch_assoc($resultO);
+$current_mode = $rowO['status'];
+
+
+$sql = "SELECT * FROM LED_status;";
+$result   = mysqli_query($conn, $sql);
+$row  = mysqli_fetch_assoc($result);
+?>
+
+
